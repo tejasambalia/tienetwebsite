3caa3660c1e09e1556bb72b33b306f0f47115d9f
tejasambalia <tejas.ambalia1994@gmail.com>
2018-05-11 22:42:15 +0530

    contact page added

---
+++ contact.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/home-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 28 Apr 2018 10:37:03 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>TieNet - About</title>
+    
+    <? include_once './includes/header.php' ?>
+</head>
+<body>
+    <div class="wrapper">
+    	<? include_once './includes/nav.php' ?>		
+
+
+    	<!-- Page Header Start -->
+        <div class="page--header pt--60 pb--60 text-center" data-bg-img="img/page-header-img/bg.jpg" data-overlay="0.85">
+            <div class="container">
+                <div class="title">
+                    <h2 class="h1 text-white">Contact</h2>
+                </div>
+
+                <ul class="breadcrumb text-gray ff--primary">
+                    <li><a href="home-1.html" class="btn-link">Home</a></li>
+                    <li class="active"><span class="text-primary">Contact</span></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- Contact Section Start -->
+        <div class="contact--section pt--80 pb--20">
+            <div class="container">
+                <!-- Map Start -->
+                <div class="map mb--80" data-trigger="map" data-map-options='{"latitude": "23.790546", "longitude": "90.375583", "zoom": "16", "api_key": "AIzaSyBK9f7sXWmqQ1E-ufRXV3VpXOn_ifKsDuc"}'></div>
+                <!-- Map End -->
+
+                <div class="row">
+                    <div class="col-md-3 pb--60">
+                        <!-- Contact Info Items Start -->
+                        <div class="contact-info--items" data-scroll-reveal="group">
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info--Item">
+                                <div class="title">
+                                    <h3 class="h4"><i class="mr--10 fa fa-map-o"></i>Address :</h3>
+                                </div>
+
+                                <div class="content fs--14 text-darker mt--4">
+                                    <p>3334 Radford Street Louisville, Victoria Melbourne, Australia</p>
+                                </div>
+                            </div>
+                            <!-- Contact Info Item End -->
+
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info--Item">
+                                <div class="title">
+                                    <h3 class="h4"><i class="mr--10 fa fa-envelope-o"></i>E-mail :</h3>
+                                </div>
+
+                                <div class="content fs--14 text-darker mt--4">
+                                    <p><a href="http://themelooks.us/cdn-cgi/l/email-protection#573e3931381733383a363e397934383a" class="btn-link"><span class="__cf_email__" data-cfemail="234a4d454c63474c4e424a4d0d404c4e">[email&#160;protected]</span></a>, <a href="http://themelooks.us/cdn-cgi/l/email-protection#d6a5a3a6a6b9a4a296b2b9bbb7bfb8f8b5b9bb" class="btn-link"><span class="__cf_email__" data-cfemail="30434540405f424470545f5d51595e1e535f5d">[email&#160;protected]</span></a></p>
+                                </div>
+                            </div>
+                            <!-- Contact Info Item End -->
+
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info--Item">
+                                <div class="title">
+                                    <h3 class="h4"><i class="mr--10 fa fa-phone"></i>Telephone :</h3>
+                                </div>
+
+                                <div class="content fs--14 text-darker mt--4">
+                                    <p><a href="tel:(+00)123123456" class="btn-link">(+00) 123123456</a>, <a href="tel:(+00)123123456" class="btn-link">(+00) 123123456</a></p>
+                                </div>
+                            </div>
+                            <!-- Contact Info Item End -->
+                        </div>
+                        <!-- Contact Info Items End -->
+                    </div>
+
+                    <div class="col-md-9 pb--60">
+                        <!-- Contact Form Start -->
+                        <div class="contact--form" data-form="ajax">
+                            <div class="contact--title">
+                                <h3 class="h4">Drop Us A Line</h3>
+                            </div>
+
+                            <div class="contact--subtitle pt--15">
+                                <h4 class="h6 fw--400 text-darkest">Don’t worry ! your e-mail address will not published.</h4>
+                            </div>
+
+                            <div class="contact--notes ff--primary mt--2">
+                                <p>(Required field are marked *)</p>
+                            </div>
+
+                            <form action="http://themelooks.us/demo/socifly/html/forms/contact-form.php" method="post">
+                                <div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Name *" class="form-control" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="email" name="email" placeholder="E-mail *" class="form-control" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="subject" placeholder="Subject *" class="form-control" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xs-12">
+                                        <div class="form-group">
+                                            <textarea name="message" placeholder="Message *" class="form-control" required></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xs-12">
+                                        <button type="submit" class="btn btn-primary mt--10">Send Message</button>
+                                    </div>
+                                </div>
+
+                                <div class="status"></div>
+                            </form>
+                        </div>
+                        <!-- Contact Form End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact Section End -->
+
+
+		<? include_once './includes/footer.php'; ?>
+    </div>
+    <!-- Wrapper End -->
+
+    <? include_once './includes/scripts.php' ?>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/home-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 28 Apr 2018 10:37:30 GMT -->
+</html>
